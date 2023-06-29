647c0620f8ab2d632a78d806bfb1915e28d64ea6
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2023-06-29 14:36:46 +0800

    Make scroll bar transparent and add section1 background and slow bakcground scroll.

---
+++ script.js
@@ -0,0 +1,11 @@
+function slowScroll(element, target) {
+  element.onscroll = () => {
+    const scrolltoTop = document.scrollingElement.scrollTop;
+    const xvalue = "center";
+    const scrollFactor = 0.3;
+    const yValue = scrolltoTop * scrollFactor;
+    target.style.backgroundPosition = xvalue + " " + yValue + "px";
+  };
+}
+
+slowScroll(document.querySelector("body"), document.querySelector(".section1"));
