7c5b4572ae8b7badafef1936b1392a397f4f920e
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2023-06-29 20:04:51 +0800

    Add collapse to nav bar.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function slowScroll(element, target) {
   element.onscroll = () => {
     const scrolltoTop = document.scrollingElement.scrollTop;
+
     const xvalue = "center";
     const scrollFactor = 0.3;
     const yValue = scrolltoTop * scrollFactor;
